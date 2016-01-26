db5ecf1eee9488cdb8f84522e53aed43eea815f9
Vitor Alano de Ataides <bundz@coconutz>
2016-01-26 16:12:35 -0200

    Saving.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 define(["jquery", "lib/spectre/SpectreController", "lib/inputs/InputsController"], function ($, SpectreController, InputsController) {
     
-    var spectre = new SpectreController(400, 200);
+    var spectre = new SpectreController(2835, 4289);
     
     var inputs = new InputsController(spectre);
     
+    var auxImage = new Image(); 
+    auxImage.src = 'imgs/Mona_Lisa.jpg';
+    auxImage.onload = function () {
+        
+        var context = spectre.view.main[0].getContext('2d');
+        
+        context.drawImage(auxImage, 0, 0);
+    };
+    
+    
 });
+ 
