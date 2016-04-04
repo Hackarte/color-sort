06f08aa2046e412738ff5c7c927eb69116202399
Vitor Alano <alano.vitor@gmail.com>
2016-04-04 14:21:34 -0300

    Working with images now :D

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 define(["jquery", "lib/spectre/SpectreController", "lib/inputs/InputsController"], function ($, SpectreController, InputsController) {
     
-    var spectre = new SpectreController(2835, 4289);
+    var spectre = new SpectreController(507, 767, 'imgs/mona.jpg');
     
     var inputs = new InputsController(spectre);
-    
-    var auxImage = new Image(); 
-    auxImage.src = 'imgs/Mona_Lisa.jpg';
-    auxImage.onload = function () {
-        
-        var context = spectre.view.main[0].getContext('2d');
-        
-        context.drawImage(auxImage, 0, 0);
-    };
-    
     
 });
  
