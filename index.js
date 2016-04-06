48e4b6e970e7a9cfab8b1aad8ee62e5cb4814a08
Vitor Alano <alano.vitor@gmail.com>
2016-04-06 17:00:04 -0300

    Fixing bubble sort

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-define(["jquery", "lib/spectre/SpectreController", "lib/inputs/InputsController"], function ($, SpectreController, InputsController) {
+define(["jquery", "lib/spectre/SpectreController", "lib/inputs/InputsController","lib/utils/Sort"], function ($, SpectreController, InputsController, sort) {
     
     var spectre = new SpectreController(507, 767, 'imgs/mona.jpg');
     
     var inputs = new InputsController(spectre);
-    
 });
  
